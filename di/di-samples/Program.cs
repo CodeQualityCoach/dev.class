653737273661c5744b3a2d40f5d194a73260ee32
Thomas Ley <thomas-ley@outlook.de>
2019-04-16 09:44:04 +0200

    code sample project added

---
+++ di/di-samples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ci_samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
